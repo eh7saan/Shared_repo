e830570ac6a7a3007ed0354419d93220dff058f8
AdilAR <65281357+AdilAbdulRahim@users.noreply.github.com>
2025-01-24 10:14:03 -0400

    Update operations.py
    Added multiply function

--- operations.py
+++ operations.py
@@ -4,3 +4,14 @@ y = input("Type another number: ")
 sum = int(x) + int(y)
 
 print("The sum is: ", sum)
+
+
+print("Now multiplication")
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+product = int(x) * int(y)
+
+print("The product is: ", product)
+
+
